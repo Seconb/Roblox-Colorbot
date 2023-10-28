89e5d591726581ac43aee1ae9a0f25681472581e
Sophia <59410102+TaylorIsBlue@users.noreply.github.com>
2023-10-28 22:11:04 +0000

    nvm lol

--- runtime-hook.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), "_internal"))  #most likely a easier way to clean up pyinstaller, but this is what i'm doing.
-
-#source/solution from https://unafaltadecomprension.blogspot.com/2014/07/pyinstaller-separating-executable-from.html
