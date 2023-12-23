707137b669ae1de87b824bff26db5ddfef073158
seconb 2nbb <75948295+Seconb@users.noreply.github.com>
2023-12-23 13:22:52 -0500

    updated installer to work and be easy! just run as admin

--- firstinstall.bat
+++ firstinstall.bat
@@ -1,21 +1,33 @@
 @echo off
+title Installing Python...
 
-:: Download the installer
-powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.11.7/python-3.11.7-amd64.exe"
-
-:: Wait for download completion
-:check_download
-if not exist python-installer.exe goto :check_download
-
-:: Install Python silently
-python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-
-:: Check if Python is installed and in PATH
-where python
-if %errorlevel% neq 0 (
-    echo Error: Python not found in PATH.
-    pause
+:: Check if Python 3.11 is installed
+python --version | findstr "3.11"
+if %errorlevel%==0 (
+    echo Python 3.11 is already installed
 ) else (
-    echo Python installed successfully and added to PATH!
-    pause
+    echo Python 3.11 is not installed. Downloading...
+    certutil -urlcache -split -f "https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe" python-3.11.exe
+    echo Installing Python 3.11...
+    start /wait python-3.11.exe /quiet InstallAllUsers=1 Include_test=0
+    del python-3.11.exe
+    echo Python 3.11 has been installed
 )
+
+:: Change directory to the current directory
+cd /d "%~dp0"
+
+:: Install requirements
+echo Installing Requirements...
+
+:: List of packages to install
+set PACKAGES=keyboard Pillow mss configparser opencv-python numpy colorama pywin32
+
+:: Install each package silently
+for %%i in (%PACKAGES%) do (
+    echo Installing %%i...
+    python -m pip install %%i --quiet
+)
+
+echo Requirements installed! Ready for use!
+pause
