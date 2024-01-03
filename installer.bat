b7f830bcad585e898f8877590569c08d036765a0
seconb 2nbb <75948295+Seconb@users.noreply.github.com>
2024-01-03 17:03:34 -0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,6 +1,10 @@
 @echo off
 title Seconb Colorbot Installer!
 echo Installing requirements for the colorbot... make sure Python is downloaded and you checked "Add Python.exe to PATH" before installing!
-pip install keyboard Pillow mss configparser opencv-python numpy colorama pywin32 pygetwindow
+
+python.exe -m pip install -q --upgrade pip
+
+pip install -q keyboard Pillow mss configparser opencv-python numpy colorama pywin32 pygetwindow
+
 echo Requirements installed! If aimsource.py crashes, manually run "pip install keyboard Pillow mss configparser opencv-python numpy colorama pywin32 pygetwindow".
 pause
