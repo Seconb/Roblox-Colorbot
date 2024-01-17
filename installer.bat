20127ff6f569bc08c5475c5b4ce9c8ab46d6d2cc
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2024-01-17 21:49:22 +0300

    Update installer.bat
    Removed Pillow module installation, since its unused in the actual colorbot

--- installer.bat
+++ installer.bat
@@ -4,7 +4,7 @@ echo Installing requirements for the colorbot... make sure Python is downloaded 
 
 python.exe -m pip install -q --upgrade pip
 
-pip install -q keyboard Pillow mss configparser opencv-python numpy colorama pywin32 pygetwindow
+pip install -q keyboard mss configparser opencv-python numpy colorama pywin32 pygetwindow
 
-echo Requirements installed! If aimsource.py crashes, manually run "pip install keyboard Pillow mss configparser opencv-python numpy colorama pywin32 pygetwindow".
+echo Requirements installed! If aimsource.py crashes, manually run "pip install keyboard mss configparser opencv-python numpy colorama pywin32 pygetwindow".
 pause
