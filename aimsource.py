a60d2d4f0bac9fc505dc24e04385937749f7612e
bora <75948295+Seconb@users.noreply.github.com>
2024-04-03 16:33:25 -0400

    Optimized heavily, improved keybinds

--- aimsource.py
+++ aimsource.py
@@ -1,36 +1,50 @@
-import keyboard # Library that relates to reading and writing keyboard inputs
-import os
+from keyboard import is_pressed # Library that relates to reading and writing keyboard inputs
+from os import system, chdir
+from os.path import dirname, join
 import mss # Takes screenshot
 import configparser
-import cv2 # Reads through screenshot
+from cv2 import dilate, threshold, findContours, RETR_EXTERNAL, CHAIN_APPROX_NONE, contourArea, cvtColor, COLOR_BGR2HSV, inRange, THRESH_BINARY # examines screenshot
 import numpy as np # Works with CV2
 import win32api # Windows API that I just use for mouse button keybinds and mouse movement to an enemy
-import win32con
 from threading import Thread
 from colorama import Fore, Style # Makes the colorful text in the console
-import ctypes # Also Windows API to move the mouse
-from time import time, sleep # Allows for specific time delays and such
+from time import time, sleep, strftime, localtime # Allows for specific time delays and such
 import pygetwindow as gw # Only takes screenshots when youre actually playing
 from urllib.request import urlopen
 from webbrowser import open as openwebpage
 from math import sqrt
 import sys
-user32 = ctypes.windll.user32
+from keybinds import *
 kernel = np.ones((3, 3), np.uint8) # 3x3 array of 1s for structuring purposes
 toggleholdmodes = ("Hold", "Toggle") #this is a tuple of [0, 1] where hold is 0, toggle is 1. 
 #importing all the modules we need to run the code.
 
+def log_error(error_message):
+    timestamp = strftime('%Y-%m-%d %H:%M:%S', localtime())
+    log_entry = f"{timestamp}: {error_message}\n"
+    try:
+        with open(error_log_path, 'a') as log_file:
+            log_file.write(log_entry)
+    except FileNotFoundError:
+        with open(error_log_path, 'w') as log_file:
+            log_file.write(log_entry)
+
 try: # if the user is running the exe, find the config and time they last opened the file relative to the exe, else do it relative to the .py file.
     if getattr(sys, 'frozen', False):
-        application_path = os.path.dirname(sys.executable)
-        config_file_path = os.path.join(application_path, 'config.txt')
-        last_launch_path = os.path.join(application_path, 'lastlaunch.txt')
+        application_path = dirname(sys.executable)
+        config_file_path = join(application_path, 'config.txt')
+        last_launch_path = join(application_path, 'lastlaunch.txt')
+        error_log_path = join(application_path, "log.txt")
+        chdir(application_path)
     else:
-        config_file_path = os.path.join(os.path.dirname(__file__), "config.txt")
-        last_launch_path = os.path.join(os.path.dirname(__file__), "lastlaunch.txt")
+        script_directory = dirname(__file__)
+        config_file_path = join(script_directory, "config.txt")
+        last_launch_path = join(script_directory, "lastlaunch.txt")
+        error_log_path = join(script_directory, "log.txt")
+        chdir(script_directory)
 except Exception as e:
     print(f"An error occurred checking if you're using the .py or the .exe: {e}")
-    sleep(5)
+    log_error(e)
     exit()
 
 try:
@@ -49,12 +63,13 @@ except:
     openwebpage("https://discord.gg/nDREsRUj9V")
 
 try: # checks for updates using the version number we defined earlier, pasted from andrewdarkyy cuz im lazy and his colorbot is just a modded version of mine so like who cares
-    if not "10" in urlopen("https://raw.githubusercontent.com/Seconb/Arsenal-Colorbot/main/version.txt").read().decode("utf-8"):
+    if not "11" in urlopen("https://raw.githubusercontent.com/Seconb/Arsenal-Colorbot/main/version.txt").read().decode("utf-8"):
         print("Outdated version, redownload: https://github.com/Seconb/Arsenal-Colorbot/releases")
         while True:
             sleep(0.1)
 except Exception as e:
     print("Error checking update: ", e)
+    log_error(e)
     print("Continuing anyway!")
     sleep(5)
     pass
@@ -65,12 +80,19 @@ try:
     config.read(config_file_path)
 except Exception as e:
         print("Error reading config:", e)
+        log_error(e)
+        exit()
 
 def rbxfocused():
     try:
         return "Roblox" == gw.getActiveWindow().title
-    except:
+    except AttributeError:
+        # if youre in the middle of alt tabbing it screws things up, so we'll just ignore if youre doing that
         return False
+    except Exception as e:
+        print("An error occurred checking if Roblox is focused: ", e)
+        log_error(e)
+        exit()
 
 def change_config_setting(setting_name, new_value): #changing the config settings ... duh.
     try:
@@ -81,11 +103,13 @@ def change_config_setting(setting_name, new_value): #changing the config setting
         print(f"Config setting '{setting_name}' changed to {new_value}")
     except Exception as e:
         print(f"Error changing config setting '{setting_name}': {e}")
+        log_error(e)
+        exit()
 
 def load(): #loading the settings, duh.
     global sct, center, screenshot, AIM_KEY, SWITCH_MODE_KEY, FOV_KEY_UP, FOV_KEY_DOWN, CAM_FOV, AIM_OFFSET_Y, AIM_OFFSET_X, AIM_SPEED_X, AIM_SPEED_Y, upper, lower, UPDATE_KEY, AIM_FOV, BINDMODE, COLOR, colorname, TRIGGERBOT, TRIGGERBOT_DELAY, SMOOTHENING, SMOOTH_FACTOR, TRIGGERBOT_DISTANCE
     #these are essential variables that show the settings of the application.
-    os.system("title Colorbot")
+    system("title Colorbot")
     
     try: #read the config file again, just in case if the user changed the settings while the program was running.
         config = configparser.ConfigParser() #this is separating all the config options you set.
@@ -93,25 +117,11 @@ def load(): #loading the settings, duh.
         config.read(config_file_path)
     except Exception as e:
         print("Error reading config:", e)
+        log_error(e)
+        exit()
 
     try:
-        BINDMODE = config.get("Config", "BINDMODE")
-        if (
-            BINDMODE.lower() == "win32"
-            or BINDMODE.lower() == "win32api"
-            or BINDMODE.lower() == "win"
-        ):
-            AIM_KEY_STRING = config.get("Config", "AIM_KEY")
-            if "win32con" in AIM_KEY_STRING:
-                AIM_KEY = eval(AIM_KEY_STRING, {"win32con": win32con})
-            else:
-                AIM_KEY = str(AIM_KEY_STRING)
-        if (
-            BINDMODE.lower() == "keyboard"
-            or BINDMODE.lower() == "k"
-            or BINDMODE.lower() == "key"
-        ):
-            AIM_KEY = config.get("Config", "AIM_KEY")
+        AIM_KEY = config.get("Config", "AIM_KEY")
         SWITCH_MODE_KEY = config.get("Config", "SWITCH_MODE_KEY")
         UPDATE_KEY = config.get("Config", "UPDATE_KEY")
         FOV_KEY_UP = config.get("Config", "FOV_KEY_UP")
@@ -182,13 +192,17 @@ def load(): #loading the settings, duh.
 
     except Exception as e:
         print("Error loading settings:", e)
+        log_error(e)
+        exit()
 load()
 
 def lclc():
     try:
-        return win32api.GetAsyncKeyState(AIM_KEY) < 0 #checking if the aim key is pressed (mouse buttons)
+        return win32api.GetAsyncKeyState(get_keycode(AIM_KEY)) < 0 #checking if the aim key is pressed
     except Exception as e:
         print("Error checking key state:", e)
+        log_error(e)
+        exit()
 
 class trb0t:
     def __init__(self): #initialize the code, first set the variables for default settings.
@@ -201,12 +215,12 @@ class trb0t:
         oldclicks = self.__clicks
         sleep(.05)
         if self.__clicks == oldclicks:
-            user32.mouse_event(0x0004)
+            win32api.mouse_event(0x0004)
 
     def __delayedaim(self):
         self.__shooting = True
         sleep(TRIGGERBOT_DELAY / 1000)
-        user32.mouse_event(0x0002)
+        win32api.mouse_event(0x0002)
         self.__clicks += 1
         Thread(target = self.__stop).start()
         self.__shooting = False
@@ -215,15 +229,13 @@ class trb0t:
         if rbxfocused():
             try: 
                 img = np.array(sct.grab(screenshot)) #grab screenshot
-                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #grab hsv color format
-                mask = cv2.inRange(hsv, lower, upper) # create a mask of only the enemy colors
-                dilated = cv2.dilate(mask, kernel, iterations=5) # dilation makes objects appear larger for the aimbot
-                thresh = cv2.threshold(dilated, 60, 255, cv2.THRESH_BINARY)[1] # threshold
-                (contours, hierarchy) = cv2.findContours(
-                    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE # find contours
-                )
+                hsv = cvtColor(img, COLOR_BGR2HSV) #grab hsv color format
+                mask = inRange(hsv, lower, upper) # create a mask of only the enemy colors
+                dilated = dilate(mask, kernel, iterations=5) # dilation makes objects appear larger for the aimbot
+                thresh = threshold(dilated, 60, 255, THRESH_BINARY)[1] # threshold
+                (contours, hierarchy) = findContours(thresh, RETR_EXTERNAL, CHAIN_APPROX_NONE) # finds the contours
                 if len(contours) != 0: # if enemies are on screen: (or if there are contours of enemies on screen)
-                    contour = max(contours, key=cv2.contourArea)
+                    contour = max(contours, key=contourArea)
                     topmost = tuple(contour[contour[:, :, 1].argmin()][0]) #finds the highest contour vertically (highest point of the enemy, their head)
                     x = topmost[0] - center + AIM_OFFSET_X # calculating the perfect center of the enemy's head by offsetting it a set amount of pixels
                     y = topmost[1] - center + AIM_OFFSET_Y
@@ -235,42 +247,45 @@ class trb0t:
                         y2 = int(y2)
                         if SMOOTHENING.lower() != "disabled":
                             if distance >= SMOOTH_FACTOR:
-                                user32.mouse_event(0x0001, x2, y2, 0, 0) #move the mouse towards, usually should feel like aimassist.
+                                win32api.mouse_event(0x0001, x2, y2, 0, 0) #move the mouse towards, usually should feel like aimassist.
                         else:
-                            user32.mouse_event(0x0001, x2, y2, 0, 0) #move the mouse towards, usually should feel like aimassist.
+                            win32api.mouse_event(0x0001, x2, y2, 0, 0) #move the mouse towards, usually should feel like aimassist.
                         if TRIGGERBOT != "disabled" and distance <= TRIGGERBOT_DISTANCE:
                             if TRIGGERBOT_DELAY != 0:
                                 if self.__shooting == False:
                                     Thread(target = self.__delayedaim).start()
                             else:
-                                user32.mouse_event(0x0002)
+                                win32api.mouse_event(0x0002)
                                 self.__clicks += 1
                                 Thread(target = self.__stop).start()
                         
             except Exception as e:
                 print("Error in processing:", e)
+                log_error(e)
+                exit()
 
     def AIMtoggle(self):
         try:
             self.AIMtoggled = not self.AIMtoggled
-            sleep(0.1) # very short cooldown to stop it from thinking we're rapid toggling.
+            sleep(0.15) # very short cooldown to stop it from thinking we're rapid toggling.
         except Exception as e:
-            print("Error toggling AIM:", e)
+            print("Error toggling aim:", e)
+            log_error(e)
+            exit()
 
     def modeswitch(self): #switch the modes from again, the array, from 0 to 1, 0 being hold, 1 being toggle.
         try:
-            if self.switchmode == 0:
-                self.switchmode = 1
-            elif self.switchmode == 1:
-                self.switchmode = 0
-            sleep(0.1)
+            self.switchmode = 1 - self.switchmode
+            sleep(0.15)
         except Exception as e:
             print("Error switching modes:", e)
+            log_error(e)
+            exit()
 
 
 def print_banner(b0t: trb0t):  # Printing the information
     try:
-        os.system("cls")  # First clearing the terminal, to then re-print with the new information. Note the colorama formatting with styling and colors!
+        system("cls")  # First clearing the terminal, to then re-print with the new information. Note the colorama formatting with styling and colors!
         print(
             f"{Style.BRIGHT}{Fore.CYAN}\n"
             f"    _   ___  ___ ___ _  _   _   _       ___ ___  _    ___  ___ ___  ___ _____\n"
@@ -323,6 +338,8 @@ def print_banner(b0t: trb0t):  # Printing the information
         )
     except Exception as e:
         print("Error printing banner:", e)
+        log_error(e)
+        exit()
 
 
 if __name__ == "__main__":
@@ -331,65 +348,38 @@ if __name__ == "__main__":
         print_banner(b0t) #to update information or print initial info.
         while True:
             # under each if statement, we first check if the key is set to disabled (if it is disabled, then it will not function. this allows the user to disable keys they don't wish to use.
-            if SWITCH_MODE_KEY != "disabled" and keyboard.is_pressed(SWITCH_MODE_KEY):
+            if SWITCH_MODE_KEY != "disabled" and is_pressed(SWITCH_MODE_KEY):
                 b0t.modeswitch() #switching the mode if the user presses the switch mode key AND its not disabled.
                 print_banner(b0t) #updating the information
-            if FOV_KEY_UP != "disabled" and keyboard.is_pressed(FOV_KEY_UP):
+            if FOV_KEY_UP != "disabled" and is_pressed(FOV_KEY_UP):
                 change_config_setting("AIM_FOV", AIM_FOV+5) #same thing as before, just adding 5 increments to the fov.
                 print_banner(b0t)
-            if FOV_KEY_DOWN != "disabled" and keyboard.is_pressed(FOV_KEY_DOWN):
+            if FOV_KEY_DOWN != "disabled" and is_pressed(FOV_KEY_DOWN):
                 change_config_setting("AIM_FOV", AIM_FOV-5) #same thing as before just removing 5 increments
                 print_banner(b0t)
-            if UPDATE_KEY != "disabled" and keyboard.is_pressed(UPDATE_KEY):
+            if UPDATE_KEY != "disabled" and is_pressed(UPDATE_KEY):
                 load() #updating the settings if the user presses the update key.
                 print_banner(b0t)
-            
 
-            sleep(0.1) #.1s cooldown as a way of preventing lag and mispresses
-
-            if (
-                BINDMODE.lower() == "win32"
-                or BINDMODE.lower() == "win32api"
-                or BINDMODE.lower() == "win" #make all strings lowercase just in case if someone in config typed it out as WIN32API, which the code wouldn't recognize.
-            ): # this is mostly for the mouse buttons.
-                if lclc(): #if user is holding down on the key or a key.
-                    if b0t.switchmode == 0: #if mode is on [**0**, 1] (means if 0) which is hold.
-                        while lclc(): #while the user is holding the key.
-                            if not b0t.AIMtoggled: 
-                                b0t.AIMtoggle() #and if the aim isn't already activated, activate it.
-                                print_banner(b0t) #update info
-                                while b0t.AIMtoggled: 
-                                    b0t.process() #while it is on/activated THEN process all screen capture, note that it doesn't process information unless activated.
-                                    if not lclc(): 
-                                        b0t.AIMtoggle() #if user stops holding the key, it'll turn off the colorbot.
-                                        print_banner(b0t) #update info.
-                    if b0t.switchmode == 1: #if mode is on [0, **1**] (means if toggled)
-                        b0t.AIMtoggle() # activate it forever until user presses again.
-                        print_banner(b0t)
-                        while b0t.AIMtoggled: #while it is toggled
-                            b0t.process() # process the images.
-                            if lclc():
-                                b0t.AIMtoggle() # if user presses the button, then deactivate
-                                print_banner(b0t) #update info
-            else:
-                if keyboard.is_pressed(AIM_KEY): #else if the user uses keyboard config, then look for keyboard buttons instead.
-                    if b0t.switchmode == 0:
-                        while keyboard.is_pressed(AIM_KEY): # SAME EXACT PROCESS AS THE MOUSE KEY PRESSES ABOVE, REFER THERE.
-                            if not b0t.AIMtoggled:
-                                b0t.AIMtoggle()
-                                print_banner(b0t)
-                                while b0t.AIMtoggled:
-                                    b0t.process()
-                                    if not keyboard.is_pressed(AIM_KEY):
-                                        b0t.AIMtoggle()
-                                        print_banner(b0t)
-                    if b0t.switchmode == 1: 
-                        b0t.AIMtoggle() # SAME EXACT PROCESS AS THE MOUSE KEY PRESSES ABOVE, REFER THERE.
-                        print_banner(b0t)
-                        while b0t.AIMtoggled:
-                            b0t.process()
-                            if keyboard.is_pressed(AIM_KEY):
-                                b0t.AIMtoggle()
-                                print_banner(b0t)
+            if lclc():  # If user is holding down on the key or a key.
+                if b0t.switchmode == 0 and not b0t.AIMtoggled:  # If mode is on [0, 1] (means if 0) which is hold.
+                    b0t.AIMtoggle()  # If the aim isn't already activated, activate it.
+                    print_banner(b0t)  # Update info.
+                    while lclc() and b0t.AIMtoggled:  # While the user is holding the key and aim is toggled.
+                        b0t.process()  # Process screen capture.
+                    if not lclc():  # If user stops holding the key.
+                        b0t.AIMtoggle()  # Turn off the colorbot.
+                        print_banner(b0t)  # Update info.
+                elif b0t.switchmode == 1:  # If mode is on [0, 1] (means if toggled).
+                    b0t.AIMtoggle()  # Activate it forever until user presses again.
+                    print_banner(b0t)  # Update info.
+                    while b0t.AIMtoggled:  # While it is toggled.
+                        b0t.process()  # Process the images.
+                        if lclc():  # If user presses the button.
+                            b0t.AIMtoggle()  # Deactivate.
+                            print_banner(b0t)  # Update info.
+            sleep(0.03) # stops lag
     except Exception as e:
         print("An error occurred:", e) #the end, DM befia on discord if you need clarity. Info by, duh, befia or taylor.
+        log_error(e)
+        exit()
